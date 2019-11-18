e19d64108badbca9062bf7d104b59525090c76aa
Matthew Sonnenberg <mds2252@columbia.edu>
2019-11-18 17:27:58 -0500

    finish tutorial 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'psw^#(d-t-axr@fsbh$hzbm$j__l56m!10vky5+3$(!p$j)nt^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
